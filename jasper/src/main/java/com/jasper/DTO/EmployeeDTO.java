1143b917c58c6cdea2f63f1af834cc3fca1a10ff
Raj Kiran Maurya <60294812+Rajkiran426@users.noreply.github.com>
2023-08-15 19:39:03 +0530

    Add files via upload

---
+++ jasper/src/main/java/com/jasper/DTO/EmployeeDTO.java
@@ -0,0 +1,18 @@
+package com.jasper.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDTO {
+    private Long id;
+    private String name;
+    private String phone;
+    private double salary;
+
+
+}
+
